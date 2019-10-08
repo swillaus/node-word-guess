24780d227763924431a44aec48d6fc3a59868406
Your Name <sam@upsidebusinessgroup.com.au>
2019-10-08 19:01:56 -0400

    comit update

--- game.js
+++ game.js
@@ -4,29 +4,35 @@ var rl = require('readline').createInterface({
     output: process.stdout
 });
 
-function Game(word){
+function Game(word) {
     this.word = new Word(word);
     this.guesses = 10;
     this.activeGame = true;
 
-    this.guessLetter = function(guess){
+    this.guessLetter = function (guess) {
         this.word.guessLetter(guess)
+        this.word.toString()
         this.guess--
-        if(this.Word.isComplete() || this.guess === 0){
+        if (this.word.isComplete() || this.guess === 0) {
+            console.log("Here")
             this.activeGame = false;
         }
     }
 
-    this.start = function(){
-        while(this.activeGame = true){
-            rl.question("Guess a letter: ", function(guess){
-                this.guessLetter(guess);
-                rl.close()
+    this.start = function () {
+        var thisObj = this;
+        if (this.activeGame) {
+            rl.question("Guess a letter: ", function (guess) {
+                thisObj.guessLetter(guess);
+                thisObj.start()
             })
+
+        } else {
+            rl.close()
+
         }
     }
 }
 
 var myGame = new Game('kazoo');
 myGame.start()
-
